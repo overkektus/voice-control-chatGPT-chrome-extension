fb57bbb26fd99ee91bc2474d493270a244bab7f7
Yahor Piskunou <egor.piskunou@gmail.com>
2023-05-31 18:25:16 +0200

    update Popup content

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,25 +1,42 @@
+import { Divider } from "@mui/material";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 import React from "react";
-import logo from "@assets/img/logo.svg";
-import "@pages/popup/Popup.css";
 
 const Popup = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
+    <Card>
+      <CardContent>
+        <Typography variant="h5" fontWeight="bold">
+          Voice Control for ChatGPT
+        </Typography>
+        <Divider />
+        <Typography variant="h6" component="div">
+          Keyboard shortcuts
+        </Typography>
+        <ul>
+          <li>
+            Press-and-hold SPACE (outside text input) to record, and release to
+            submit
+          </li>
+          <li>Press ESC or Q to cancel a transcription</li>
+          <li>
+            Press E to stop and copy the transcription to the ChatGPT input
+            field without submitting
+          </li>
+        </ul>
+        <Divider />
+      </CardContent>
+      <CardActions>
+        <Typography variant="h6" component="div">
+          Need help?
+        </Typography>
+        <Typography>Please fill out this form or write an email</Typography>
+      </CardActions>
+    </Card>
   );
 };
 
