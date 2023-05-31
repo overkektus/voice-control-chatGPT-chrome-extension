74d89c809b1c216d30ca7089b29cd72f9bd78f2f
Yahor Piskunou <egor.piskunou@gmail.com>
2023-05-31 20:47:18 +0200

    unse Button and Menu components in App

--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import Button from "./Button";
+import Menu from "./Menu";
 
 export default function App() {
-  useEffect(() => {
-    console.log("content view loaded");
-  }, []);
-
-  return <div className="content-view">content view</div>;
+  return (
+    <div className="content-view flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+      <Button />
+      <Menu />
+    </div>
+  );
 }
