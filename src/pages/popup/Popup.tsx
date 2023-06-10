1a6d8e5362de9ad3ebf385eff9ed3d85da2a13c0
Yahor Piskunou <egor.piskunou@gmail.com>
2023-06-10 18:09:21 +0200

    add some styles to Popup

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,43 +1,74 @@
-import { Divider } from "@mui/material";
-import Button from "@mui/material/Button";
+import { Divider, Stack, Link } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import React from "react";
+import emotion from "@emotion/styled";
 
 const Popup = () => {
   return (
-    <Card>
+    <Card sx={{ padding: "0.8rem" }}>
       <CardContent>
-        <Typography variant="h5" fontWeight="bold">
+        <Typography variant="h2" fontWeight="bold" sx={{ fontSize: "1.4rem" }}>
           Voice Control for ChatGPT
         </Typography>
-        <Divider />
-        <Typography variant="h6" component="div">
+        <Divider sx={{ margin: "0.7rem 0" }} />
+        <Typography
+          variant="h3"
+          fontWeight="bold"
+          component="div"
+          sx={{ fontSize: "1rem" }}
+        >
           Keyboard shortcuts
         </Typography>
-        <ul>
-          <li>
-            Press-and-hold SPACE (outside text input) to record, and release to
-            submit
-          </li>
-          <li>Press ESC or Q to cancel a transcription</li>
-          <li>
-            Press E to stop and copy the transcription to the ChatGPT input
-            field without submitting
-          </li>
-        </ul>
+        <List>
+          <ListItem>
+            Press-and-hold <ControlKey>SPACE</ControlKey> (outside text input)
+            to record, and release to submit
+          </ListItem>
+          <ListItem>
+            Press <ControlKey>ESC</ControlKey> or <ControlKey>Q</ControlKey> to
+            cancel a transcription
+          </ListItem>
+          <ListItem>
+            Press <ControlKey>E</ControlKey> to stop and copy the transcription
+            to the ChatGPT input field without submitting
+          </ListItem>
+        </List>
         <Divider />
       </CardContent>
       <CardActions>
-        <Typography variant="h6" component="div">
-          Need help?
-        </Typography>
-        <Typography>Please fill out this form or write an email</Typography>
+        <Stack direction="column">
+          <Typography
+            variant="h3"
+            fontWeight="bold"
+            component="div"
+            sx={{ fontSize: "1rem", margin: "0.5rem 0" }}
+          >
+            Need help?
+          </Typography>
+          <Typography sx={{ fontSize: "0.7rem" }}>
+            Please fill out <Link underline="hover">this form</Link> or{" "}
+            <Link underline="hover">write an email</Link>
+          </Typography>
+        </Stack>
       </CardActions>
     </Card>
   );
 };
+
+const List = emotion.ul`
+  
+`;
+
+const ListItem = emotion.li`
+  margin: 0.8rem 0;
+  line-height: 1.5;
+`;
+
+const ControlKey = emotion.span`
+  color: red;
+  font-weight: bold;
+`;
 
 export default Popup;
