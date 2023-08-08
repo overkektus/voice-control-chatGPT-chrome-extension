ee24ef0e08c22b4485ae1caf61b7fd18c3a439ac
Egor Piskunou <egor.piskunou@gmail.com>
2023-08-08 14:42:33 +0200

    add useTextToSpeech hook

--- src/hooks/useTextToSpeech.ts
+++ src/hooks/useTextToSpeech.ts
@@ -1,56 +1,30 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export function useTextToSpeech() {
-  const [textQueue, setTextQueue] = useState([]);
-  const [isSpeaking, setIsSpeaking] = useState(false);
+export const useTextToSpeech = () => {
+  const [queue, setQueue] = useState([]);
 
-  const speak = (textPart) => {
-    if (!window.speechSynthesis) {
-      console.error("SpeechSynthesis API is not supported in this browser.");
-      return;
-    }
-
-    setTextQueue((prevQueue) => [...prevQueue, textPart]);
-
-    if (!isSpeaking) {
-      startSpeaking();
-    }
+  const addToQueue = (text) => {
+    setQueue((prev) => [...prev, text]);
   };
 
-  const startSpeaking = () => {
-    if (textQueue.length > 0) {
-      const utterance = new SpeechSynthesisUtterance(textQueue[0]);
-      setIsSpeaking(true);
-
-      utterance.onend = () => {
-        setTextQueue((prevQueue) => prevQueue.slice(1));
-        if (textQueue.length > 1) {
-          startSpeaking();
-        } else {
-          setIsSpeaking(false);
-        }
-      };
-      window.speechSynthesis.speak(utterance);
-    }
+  const clearQueue = () => {
+    setQueue([]);
   };
 
-  const stopSpeaking = () => {
-    window.speechSynthesis.cancel();
-    setTextQueue([]);
-    setIsSpeaking(false);
+  const startSpeak = () => {
+    if (queue.length === 0) return;
+
+    const utterance = new SpeechSynthesisUtterance(queue[0]);
+    setQueue((prev) => prev.slice(1));
+    speechSynthesis.speak(utterance);
   };
 
   useEffect(() => {
-    if (isSpeaking && textQueue.length === 0) {
-      setIsSpeaking(false);
-    }
-  }, [isSpeaking, textQueue]);
+    startSpeak();
+  }, [queue]);
 
   return {
-    isSpeaking,
-    speak,
-    stopSpeaking,
+    addToQueue,
+    clearQueue,
   };
-}
-
-export default useTextToSpeech;
+};
