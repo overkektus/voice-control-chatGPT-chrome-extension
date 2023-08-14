fd72ff4ef283a42af780913077b626ee70520647
Egor Piskunou <egor.piskunou@gmail.com>
2023-08-14 15:31:29 +0200

    update useTextToSpeech hook

--- src/hooks/useTextToSpeech.ts
+++ src/hooks/useTextToSpeech.ts
@@ -2,29 +2,36 @@ import { useEffect, useState } from "react";
 
 export const useTextToSpeech = () => {
   const [queue, setQueue] = useState([]);
+  const [speaking, setSpeaking] = useState(false);
 
   const addToQueue = (text) => {
     setQueue((prev) => [...prev, text]);
   };
 
-  const clearQueue = () => {
-    setQueue([]);
-  };
-
   const startSpeak = () => {
     if (queue.length === 0) return;
 
-    const utterance = new SpeechSynthesisUtterance(queue[0]);
-    setQueue((prev) => prev.slice(1));
-    speechSynthesis.speak(utterance);
+    if (speaking) {
+      const utterance = new SpeechSynthesisUtterance(queue[0]);
+      setQueue((prev) => prev.slice(1));
+      speechSynthesis.speak(utterance);
+    }
+  };
+
+  const stopSpeak = () => {
+    if (speaking) {
+      speechSynthesis.cancel();
+      setSpeaking(false);
+    }
   };
 
   useEffect(() => {
+    setSpeaking(true);
     startSpeak();
   }, [queue]);
 
   return {
     addToQueue,
-    clearQueue,
+    stopSpeak,
   };
 };
